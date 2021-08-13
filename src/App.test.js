13679e95b63d98a8df23eb1a89a6446e43089214
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-08-13 12:34:25 -0400

    Enzyme setup

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import App from './App';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test('renders learn react link', () => {
+
+});
